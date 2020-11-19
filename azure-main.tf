975f241aca370c321d2a0cf7486606ee87444840
andreas-aune <74398621+andreas-aune@users.noreply.github.com>
2020-11-19 18:07:33 +0100

    Update azure-main.tf

--- azure-main.tf
+++ azure-main.tf
@@ -145,6 +145,35 @@ resource "azurerm_linux_virtual_machine" "azure-web-vm" {
     environment = var.app_environment,
     responsible = var.department_id
   }
+
+ # It's easy to transfer files or templates using Terraform.
+  provisioner "file" {
+    source      = "files/setup.sh"
+    destination = "/home/${var.linux_admin_user}/setup.sh"
+
+    connection {
+      type     = "ssh"
+      user     = var.linux_admin_user
+      password = var.linux_admin_password
+      host     = azurerm_public_ip.azure-web-ip.ip_address
+    }
+  }
+
+  # This shell script starts our Apache server and prepares the demo environment.
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/${var.linux_admin_user}/setup.sh",
+      "sudo /home/${var.linux_admin_user}/setup.sh",
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = var.linux_admin_user
+      password = var.linux_admin_password
+      host     = azurerm_public_ip.azure-web-ip.ip_address
+    }
+  }
+
 }
 
 #Output
